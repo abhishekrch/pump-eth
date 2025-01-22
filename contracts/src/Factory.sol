c099ef7aa93a98433ac56dd2ba6526fd452cee3b
Abhishek Choudhary <pmoabhishek@gmail.com>
2025-01-22 21:18:13 +0530

    feat(contracts): complete Factory.sol

--- contracts/src/Factory.sol
+++ contracts/src/Factory.sol
@@ -1,12 +1,14 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-import { Token } from "./Token.sol";
+import {Token} from "./Token.sol";
 
-contract Counter {
+contract Factory {
+    uint256 public constant TARGET = 3 ether;
+    uint256 public constant TOKEN_LIMIT = 500_000 ether;
     uint256 public fee;
     address public owner;
-    
+
     uint256 public totalTokens;
     address[] public tokens;
     mapping(address => TokenSale) public tokenToSale;
@@ -21,14 +23,27 @@ contract Counter {
     }
 
     event Created(address indexed token);
+    event Buy(address indexed token, uint256 amount);
+
 
     constructor(uint256 _fee) {
         fee = _fee;
         owner = msg.sender;
     }
 
-    function getTokenSale(uint256 _index) public view returns (TokenSale memory) {
+    function getTokenSale(
+        uint256 _index
+    ) public view returns (TokenSale memory) {
         return tokenToSale[tokens[_index]];
+    }
+
+    function getCost(uint256 _sold) public pure returns (uint256) {
+        uint256 floor = 0.0001 ether;
+        uint256 step = 0.0001 ether;
+        uint256 increment = 10000 ether;
+
+        uint256 cost = (step * (_sold / increment)) + floor;
+        return cost;
     }
 
     function create(
@@ -40,9 +55,64 @@ contract Counter {
         tokens.push(address(token));
         totalTokens++;
 
-        TokenSale memory sale = TokenSale(address(token), _name, msg.sender, 0, 0,true);
+        TokenSale memory sale = TokenSale(
+            address(token),
+            _name,
+            msg.sender,
+            0,
+            0,
+            true
+        );
 
         tokenToSale[address(token)] = sale;
         emit Created(address(token));
     }
+
+    function buy(address _token, uint256 _amount) external payable {
+        TokenSale storage sale = tokenToSale[_token];
+        
+        require(sale.isOpen == true, "Factory: Buying is closed");
+        require(_amount >= 1 ether, "Factory: Amount is too low");
+        require(_amount <= 10000 ether, "Factory: Amount exceeded");
+
+        uint256 cost = getCost(sale.sold);
+
+        uint256 price = cost * (_amount / 10 ** 18);
+
+        require(msg.value >= price, "Insufficient ETH");
+        
+        sale.sold += _amount;
+        sale.raised += price;
+
+        if (sale.sold >= TOKEN_LIMIT || sale.raised >= TARGET) {
+            sale.isOpen = false;
+        }
+
+        Token(_token).transfer(msg.sender, _amount);
+
+        emit Buy(_token, _amount);
+    }
+
+    function deposit(address _token) external {
+        Token token = Token(_token);
+        TokenSale memory sale = tokenToSale[_token];
+
+        require(sale.isOpen == false, "Factory: Target not reached");
+        
+        token.transfer(sale.creator, token.balanceOf(address(this)));
+
+        (bool success, ) = payable(sale.creator).call{value: sale.raised}("");
+        require(success, "Factory: ETH transfer Failed");
+
+    }
+
+    function withdraw(uint256 _amount) external {
+        require(msg.sender == owner, "Factory: Not owner");
+
+        (bool success, ) = payable(owner).call{value: _amount}("");
+
+        require(success, "Factory: ETH transfer failed");
+
+    }
+
 }
