b769b65be6ed835df71776e2edac0d783d7bae15
Abhishek Choudhary <pmoabhishek@gmail.com>
2025-01-29 23:38:29 +0530

    feat: add List component

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,35 +1,47 @@
-'use client';
+"use client";
 
 import { useEffect, useState } from "react";
 import Header from "./components/Header";
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
 import { CONTRACT_ABI, CONTRACT_ADDRESS } from "@/utils/Factory";
 import "./globals.css";
+import List from "./components/List";
 
 export default function Home() {
-  const [account, setAccount] = useState(null)
-  const [provider, setProvider] = useState(null)
-  const [factory, setFactory] = useState(null)
-  const [fee, setFee] = useState(0)
+  const [account, setAccount] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [factory, setFactory] = useState(null);
+  const [fee, setFee] = useState(0);
+  const [showCreate, setShowCreate] = useState(false);
+
+  function toggleCreate() {
+    showCreate ? setShowCreate(false) : setShowCreate(true);
+  }
 
   async function loadBlockchainData() {
-    const provider = new ethers.BrowserProvider(window.ethereum)
-    setProvider(provider)
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    setProvider(provider);
 
-    const network = await provider.getNetwork()
+    const network = await provider.getNetwork();
 
-    const factory = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, provider)
-    setFactory(factory)
+    const factory = new ethers.Contract(
+      CONTRACT_ADDRESS,
+      CONTRACT_ABI,
+      provider
+    );
+    setFactory(factory);
 
-    const fee = await factory.fee()
+    const fee = await factory.fee();
     console.log(fee);
-    setFee(fee)
+    setFee(fee);
   }
 
   async function checkWalletConnection() {
     try {
       if (window.ethereum) {
-        const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+        const accounts = await window.ethereum.request({
+          method: "eth_accounts",
+        });
         if (accounts.length > 0) {
           const account = ethers.getAddress(accounts[0]);
           setAccount(account);
@@ -51,12 +63,21 @@ export default function Home() {
 
       <main>
         <div className="create">
-          <button className="btn--fancy">
-              {"[ create a new token ]"}
+          <button onClick={factory && account && toggleCreate} className="btn--fancy">
+          {!factory ? (
+              "[ Contract not deployed ]"
+            ) : !account ? (
+              "[ Please Connect ]"
+            ) : (
+              "[ Start a new token ]"
+            )}
           </button>
         </div>
-        </main>
+      </main>
 
+      {showCreate && (
+        <List toggleCreate={toggleCreate} fee = {fee} provider={provider} factory={factory} />
+      )}
     </div>
-  )
+  );
 }
