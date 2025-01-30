d7f11d6788ca58cac8fb6ba36d349ad889150dbc
Abhishek Choudhary <pmoabhishek@gmail.com>
2025-01-30 11:38:39 +0530

    feat: add token display functionality

---
+++ frontend/app/components/Token.jsx
@@ -0,0 +1,19 @@
+import { ethers } from "ethers";
+
+export default function Token({ toggleTrade, token }) {
+  return (
+    <div>
+      <button onClick={() => toggleTrade(token)} className="token">
+        <div className="token__details">
+          <img src={token.image} alt="token image" width={256} height={256} />
+          <p>
+            created by{" "}
+            {token.creator.slice(0, 6) + "..." + token.creator.slice(38, 42)}
+          </p>
+          <p>market Cap: {ethers.formatUnits(token.raised, 18)} eth</p>
+          <p className="name">{token.name}</p>
+        </div>
+      </button>
+    </div>
+  );
+}
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -6,6 +6,8 @@ import { ethers } from "ethers";
 import { CONTRACT_ABI, CONTRACT_ADDRESS } from "@/utils/Factory";
 import "./globals.css";
 import List from "./components/List";
+import images from "@/utils/images";
+import Token from "./components/Token";
 
 export default function Home() {
   const [account, setAccount] = useState(null);
@@ -13,6 +15,7 @@ export default function Home() {
   const [factory, setFactory] = useState(null);
   const [fee, setFee] = useState(0);
   const [showCreate, setShowCreate] = useState(false);
+  const [tokens, setTokens] = useState([]);
 
   function toggleCreate() {
     showCreate ? setShowCreate(false) : setShowCreate(true);
@@ -32,8 +35,30 @@ export default function Home() {
     setFactory(factory);
 
     const fee = await factory.fee();
-    console.log(fee);
     setFee(fee);
+
+    const totalTokens = await factory.totalTokens();
+    const tokens = [];
+
+    for (let i = 0; i < totalTokens; i++) {
+      if (i == 6) {
+        break;
+      }
+      const tokenSale = await factory.getTokenSale(i);
+
+      const token = {
+        token: tokenSale.token,
+        name: tokenSale.name,
+        creator: tokenSale.creator,
+        sold: tokenSale.sold,
+        raised: tokenSale.raised,
+        isOpen: tokenSale.isOpen,
+        image: images[i],
+      };
+
+      tokens.push(token);
+    }
+    setTokens(tokens.reverse());
   }
 
   async function checkWalletConnection() {
@@ -63,20 +88,46 @@ export default function Home() {
 
       <main>
         <div className="create">
-          <button onClick={factory && account && toggleCreate} className="btn--fancy">
-          {!factory ? (
-              "[ Contract not deployed ]"
-            ) : !account ? (
-              "[ Please Connect ]"
-            ) : (
-              "[ Start a new token ]"
-            )}
+          <button
+            onClick={factory && account && toggleCreate}
+            className="btn--fancy"
+          >
+            {!factory
+              ? "[ Contract not deployed ]"
+              : !account
+              ? "[ Please Connect ]"
+              : "[ Start a new token ]"}
           </button>
+        </div>
+
+        <div className="listings">
+          <h1>new listings</h1>
+
+          <div className="tokens">
+            {!account ? (
+              <p>please connect wallet</p>
+            ) : tokens.length === 0 ? (
+              <p>no tokens listd</p>
+            ) : (
+              tokens.map((token, index) => (
+                <Token
+                  toggleCreate={() => {}}
+                  token={token}
+                  key={index}
+                />
+              ))
+            )}
+          </div>
         </div>
       </main>
 
       {showCreate && (
-        <List toggleCreate={toggleCreate} fee = {fee} provider={provider} factory={factory} />
+        <List
+          toggleCreate={toggleCreate}
+          fee={fee}
+          provider={provider}
+          factory={factory}
+        />
       )}
     </div>
   );
